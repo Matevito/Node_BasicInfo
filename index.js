b1756006827e9423b39d1372ff179baf108d1d36
Mateo <madiazt@unal.edu.co>
2021-06-26 21:54:48 -0500

    solved 404 bugg

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const http = require("http")
 const url = require("url")
 const fs = require("fs")
 
+const landPage_404 = fs.readFileSync("404.html", "utf-8", (err, data) => {
+    if (err) throw err;
+    return data;
+});
 
 http.createServer((req, res) => {
     const q  = url.parse(req.url, true);
@@ -14,7 +18,9 @@ http.createServer((req, res) => {
 
     fs.readFile(fileName, (err, data) => {
         if(err){
-            //todo
+            res.writeHead(404, {"Content-Type":"text/html"})
+            res.write(landPage_404)
+            return res.end()
         }
         res.writeHead(200, {"Content-Type":"text/html"});
         res.write(data)
